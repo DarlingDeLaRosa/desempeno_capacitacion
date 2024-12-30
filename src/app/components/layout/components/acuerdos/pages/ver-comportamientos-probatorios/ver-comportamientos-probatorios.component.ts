807104ba5474085876497e0dbd70940d27bde136
DarlingDeLaRosa <darling.delarosa@isfodosu.edu.do>
2024-12-29 23:40:50 -0800

    working on te comportamientos view

--- src/app/components/layout/components/acuerdos/pages/ver-comportamientos-probatorios/ver-comportamientos-probatorios.component.ts
+++ src/app/components/layout/components/acuerdos/pages/ver-comportamientos-probatorios/ver-comportamientos-probatorios.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClassImports } from '../../../../../../helpers/class.components';
+import { MaterialComponents } from '../../../../../../helpers/material.components';
+import { agreementService } from '../../services/acuerdo.service';
+import { systemInformationService } from '../../../../services/systemInformationService.service';
 
 @Component({
   selector: 'app-ver-comportamientos-probatorios',
   standalone: true,
-  imports: [],
+  imports: [MaterialComponents, ClassImports],
   templateUrl: './ver-comportamientos-probatorios.component.html',
   styleUrl: './ver-comportamientos-probatorios.component.css'
 })
-export class VerComportamientosProbatoriosComponent {
+export class VerComportamientosProbatoriosComponent implements OnInit {
 
+  constructor(
+    private agreementservice: agreementService,
+    public systemInformation: systemInformationService,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.getBehaviorsTested()
+  }
+
+  getBehaviorsTested() {
+    this.agreementservice.getBehaviorTest()
+      .subscribe((res: any) => {
+        console.log(res);
+      })
+  }
 }
